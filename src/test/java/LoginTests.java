1bc8b709beb0448946a3d35acca3fa914ba93087
Levandovskii-Lev <levlevisr@gmail.com>
2023-12-18 19:19:54 +0200

    add login test

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,36 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+@BeforeClass
+    public void preCondition(){
+        //open browser
+        //open site
+
+    }
+@Test
+    public void loginSucces(){
+        //open form
+        //fill form
+        //submit form
+    }
+@Test
+    public void loginWrongEmail(){
+        //open form
+        //fill form
+        //submit form
+    }
+@Test
+    public void loginWrongPassword(){
+        //open form
+        //fill form
+        //submit form
+    }
+
+    @AfterClass
+    public void postConditions(){
+        //close browser
+    }
+}
